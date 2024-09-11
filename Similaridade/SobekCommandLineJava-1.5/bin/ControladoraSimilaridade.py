f3b7bb457c75c6d14f0444588821a8e0fb1739b4
GabrielZoega <gabriel.zoega10@gmail.com>
2024-09-11 08:55:22 -0300

    Adicionando a opção de visualizar os Grafos dos textos

--- Similaridade/SobekCommandLineJava-1.5/bin/ControladoraSimilaridade.py
+++ Similaridade/SobekCommandLineJava-1.5/bin/ControladoraSimilaridade.py
@@ -1,6 +1,8 @@
 from SimilaridadeCossenos import SimilaridadeCossenos
 from SimilaridadeGrafos import SimilaridadeGrafos
 from controladoraSOBEK import ControladoraSOBEK
+import networkx as nx
+import matplotlib.pyplot as plt
 
 class ControladoraSimilaridade:
     
@@ -18,10 +20,23 @@ class ControladoraSimilaridade:
             if (idioma != "pt" and idioma != "en"):
                 print("Idioma Inválido!")
                 return
-            print("------------------------------------------------------------")
+            print("------------------------------------------------------------\n")
             
             texto1 = ControladoraSOBEK.gerarGrafo(texto1, idioma)
             texto2 = ControladoraSOBEK.gerarGrafo(texto2, idioma)
+            print("------------------------------------------------------------")
+            print("Deseja visualizar os grafos dos textos? \n0 - Não \n1 - Sim")
+            exebirGrafos = int(input("Digite sua Resposta: "))
+            print("------------------------------------------------------------\n")
+            
+            if(exebirGrafos == 1):
+                nx.draw(texto1, with_labels=True, font_weight='bold')
+                plt.title("Texto 1")
+                plt.show()
+                nx.draw(texto2, with_labels=True, font_weight='bold')
+                plt.title("Texto 2")
+                plt.show()
+            
             similaridade = SimilaridadeGrafos.simGrafos(texto1, texto2, idioma)
         
         return similaridade
