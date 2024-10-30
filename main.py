93ab9d43be19309ff481dd00ed00540e34512346
GabrielZoega <gabriel.zoega10@gmail.com>
2024-10-30 15:11:02 -0300

    Removendo os arquivos do cálculo da similaridade e atualizando o main

--- main.py
+++ main.py
@@ -1,47 +1,24 @@
 from sys import argv
-from Similaridade.SobekCommandLineJava.bin.ControladoraSimilaridade import ControladoraSimilaridade
 from RIT.BibliotecaRIT.BibliotecaRITFachada import BibliotecaRITFachada
 import networkx as nx
 import matplotlib.pyplot as plt
 
 
-if(len(argv) > 1):
-    path = argv[1].split(',')
-    path[0] = "Similaridade/Textos/" + path[0]
-    path[1] = "Similaridade/Textos/" + path[1]
+usuario = input("Digite o nome do usuário: ")
+repositorio = input("Digite o nome do Repositório: ")
 
-    with open(path[0], 'r') as arquivo:
-        arquivo1 = arquivo.read()
+tokens = str(input("Digite os tokens a serem utilizados: "))
+tokens = tokens.split(',')
+BibliotecaRITFachada.initTokens(tokens)
 
-    with open(path[1], 'r') as arquivo:
-        arquivo2 = arquivo.read()
+visao = int(input("Digite a visão a ser usada: "))
+pagina = int(input("Digite a página inicial: "))
 
-    print("------------------------------------------------------------")
-    print("1 - Cálculo de Similaridade de Cossenos")
-    print("2 - Cálculo de Similaridade por Grafos")
-    tipoCalculo = int(input("Digite o Tipo de Cálculo: "))
-    print("------------------------------------------------------------")
+if(visao == 4):
+    arg = input("Digite o nome do Autor: ")
+    BibliotecaRITFachada.extrairDadosGitHub(usuario, repositorio, visao, arg, pagina)
+if(visao == 5):
+    arg = input("Digite a data (yyyy-mm-dd): ")
+    BibliotecaRITFachada.extrairDadosGitHub(usuario, repositorio, visao, arg, pagina)
 
-    nivelSimilaridade = ControladoraSimilaridade.calcularSimilaridade(arquivo1, arquivo2, tipoCalculo)
-    print(nivelSimilaridade)
-
-else:
-    
-    usuario = input("Digite o nome do usuário: ")
-    repositorio = input("Digite o nome do Repositório: ")
-
-    tokens = str(input("Digite os tokens a serem utilizados: "))
-    tokens = tokens.split(',')
-    BibliotecaRITFachada.initTokens(tokens)
-
-    visao = int(input("Digite a visão a ser usada: "))
-    pagina = int(input("Digite a página inicial: "))
-    
-    if(visao == 4):
-        arg = input("Digite o nome do Autor: ")
-        BibliotecaRITFachada.extrairDadosGitHub(usuario, repositorio, visao, arg, pagina)
-    if(visao == 5):
-        arg = input("Digite a data (yyyy-mm-dd): ")
-        BibliotecaRITFachada.extrairDadosGitHub(usuario, repositorio, visao, arg, pagina)
-
-    BibliotecaRITFachada.extrairDadosGitHub(usuario, repositorio, visao,"", pagina)
+BibliotecaRITFachada.extrairDadosGitHub(usuario, repositorio, visao,"", pagina)
